669a54c07e671b17398cd8d2cfe9d0082e555ce0
AdrienPortier <149541821+AdrienPortier@users.noreply.github.com>
2024-11-07 19:53:28 +0100

    Add files via upload

--- Projet LMAT1351 2.1.py
+++ Projet LMAT1351 2.1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-P = [[0,0],[-1,2],[4,2],[6,0],[7,1.75]]
+P = np.array([[-1,0], [-0.5,15], [0, -5], [2, 0], [1, 3],[4,5],[-2,8]])
 
 def B(t,P):
     if len(P) == 2:
@@ -11,10 +11,19 @@ def B(t,P):
 
 t = np.linspace(0,1,1000)
 x,y = B(t,P)
-plt.plot(x,y)
+plt.plot(x,y, label = "Courbe de Bézier", color = "red")
+
 for p in P:
-    plt.plot([p[0]],[p[1]],"black",marker=".")
+    if (p == P[0]).all():
+        plt.scatter(p[0], p[1], color='mediumseagreen', s=100, marker='*', label="Point de départ", zorder=5)
+    elif (p == P[-1]).all():
+        plt.scatter(p[0], p[1], color='royalblue', s=100, marker='*', label="Point d'arrivée", zorder=5)
+    else:
+        plt.scatter(p[0], p[1], color='black', s=75, marker='o', zorder=5)
+
 for i in range(0,len(P)-1):
     x,y = B(t,[P[i],P[i+1]])
-    plt.plot(x,y,"--",color="red")
+    plt.plot(x,y,"--",color="black")
+plt.legend()
 plt.show()
+plt.savefig("BezierExemple.svg")
