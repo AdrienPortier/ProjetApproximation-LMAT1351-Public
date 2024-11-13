14e5fd1821114b940698dd5d28717c58f369166d
AdrienPortier <149541821+AdrienPortier@users.noreply.github.com>
2024-11-13 01:04:43 +0100

    Add files via upload

--- Projet LMAT1351 3.1-3.2-3.3_v2.py
+++ Projet LMAT1351 3.1-3.2-3.3_v2.py
@@ -20,36 +20,34 @@ def Legendre(n):
             P.append(((2*i-1)/i) * minus1 - ((i-1)/i) * minus2)
     return P
 
-# Create a list of normalized Legendre polynomials
 n = 30
 L = Legendre(n)
 
 # Coefficients
-A = [sp.Poly(L[i], x).all_coeffs() for i in range(n)]
+A = [sp.Poly(L[i], x).all_coeffs() for i in range(n+1)]
 C = [[float(e) for e in coeffs] for coeffs in A]
 
 X = np.linspace(-1,1,1000)
 plt.figure(figsize=(10, 6))
 for i in range(n+1):
-    # Convertir le polynôme NumPy en liste pour le passer à mp.polyval
     poly_coeffs = list(L[i])
-    Y = [mp.polyval(poly_coeffs, x) for x in X]  # Evaluation du polynôme
+    Y = [mp.polyval(poly_coeffs, x) for x in X]
     plt.plot(X, Y)
 
-plt.title('Polynômes de Legendre normalisés')
+plt.title('Polynômes de Legendre ($p_n$) pour n allant de 1 à 30')
 plt.xlabel('x')
-plt.ylabel('P_n(x)')
+plt.ylabel('$p_n(x)$')
 plt.grid(True)
+plt.savefig("3.2_rep.svg")
 plt.show()
 
 
-# Adjust precision for numerical calculations
 mp.dps = 10
 
 Roots = []
 for coeffs in C:
     try:
-        r = np.roots(coeffs)
+        r = polyroots(coeffs)
     except:
         r = sp.Poly(coeffs, x).all_roots()
     Roots.append([float(r_i) for r_i in r])
@@ -58,16 +56,20 @@ for coeffs in C:
 all_roots = np.concatenate(Roots)
 
 
-# Plotting
+# Distribution
 for degree, roots in enumerate(Roots):
     plt.scatter(roots, [degree] * len(roots), color='black', s=10, label=f"Degree {degree}" if degree == 0 else "")
-
 plt.title('Distribution des racines des polynômes de Legendre')
 plt.xlabel('Racines')
 plt.ylabel('Degré du polynôme')
 plt.grid(True, linestyle='--', alpha=0.7)
+plt.savefig("3.3_distrib_racines.svg")
 plt.show()
 
-# Histogram
+# Histogramme
 plt.hist(all_roots, bins=30, density=False, alpha=0.6, color='skyblue', edgecolor='black')
+plt.ylabel("Fréquence")
+plt.xlabel("Valeurs des racines")
+plt.title("Histogramme des racines des polynômes de Legendre pour n de 1 à 30")
+plt.savefig("3.3_histo_racines.svg")
 plt.show()
